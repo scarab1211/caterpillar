7653fdc9b2b05dbc20035fd5283fa3614fef29d8
scarab1211 <99696196+scarab1211@users.noreply.github.com>
2022-02-16 00:01:41 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 
-var name = window.prompt("Enter your name: ");
-alert("Your name is " + name);
+//I was going to encrypt this but holy fuck is that complex so if you came into the source code to root out the password, Hooray! You win at life!
+var pass = "citrus";
+var element = document.getElementById(id);
+var name = window.prompt("Enter the password: ");
+if (name != pass) {
+ 	alert("Incorrect");
+	element.innerHTML = "Incorrect";
+} else {
+	alert("Correct!");
+	element.innerHTML = "Youtube link here";
+}
+
+
