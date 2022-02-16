4cd1d9f42ff3d8df9ca83b7ad129774a2f5757ca
scarab1211 <99696196+scarab1211@users.noreply.github.com>
2022-02-16 00:02:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 //I was going to encrypt this but holy fuck is that complex so if you came into the source code to root out the password, Hooray! You win at life!
 var pass = "citrus";
-var element = document.getElementById(id);
+var element = document.getElementById("pass");
 var name = window.prompt("Enter the password: ");
 if (name != pass) {
  	alert("Incorrect");
